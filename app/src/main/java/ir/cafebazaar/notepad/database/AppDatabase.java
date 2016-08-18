2238b17b93bf43e3cee845d93cbd20994fefabd5
Moh Mah <mohammadmahtabi@gmail.com>
2016-08-19 01:21:04 +0430

    Added Utils class for database

--- app/src/main/java/ir/cafebazaar/notepad/database/AppDatabase.java
+++ app/src/main/java/ir/cafebazaar/notepad/database/AppDatabase.java
@@ -1,6 +1,10 @@
 package ir.cafebazaar.notepad.database;
 
 import com.raizlabs.android.dbflow.annotation.Database;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+import ir.cafebazaar.notepad.models.Folder;
+import ir.cafebazaar.notepad.models.Note;
+import java.util.Date;
 
 /**
  * Created by MohMah on 8/17/2016.
@@ -9,4 +13,32 @@ import com.raizlabs.android.dbflow.annotation.Database;
 public class AppDatabase{
 	public static final String NAME = "AppDatabase";
 	public static final int VERSION = 1;
+
+	public static class Utils{
+		public static void createSomeFolders(int count){
+			for (int i = 0; i < count; i++){
+				Folder folder = new Folder();
+				folder.setCreatedAt(new Date());
+				folder.setId(i);
+				folder.setName("Folder " + (i + 1));
+				folder.save();
+			}
+		}
+
+		public static void createSomeNotes(int count){
+			for (int i = 0; i < count; i++){
+				Note note = new Note();
+				String ithNote = "Note " + (i + 1);
+				note.setBody(ithNote + "'s Body Text Goes Here");
+				note.setCreatedAt(new Date());
+				note.setLastModified(new Date());
+				note.setId(i);
+				note.setTitle(ithNote + "'s Title");
+			}
+		}
+
+		public static void deleteAllFolders(){
+			SQLite.delete().from(Folder.class).execute();
+		}
+	}
 }
