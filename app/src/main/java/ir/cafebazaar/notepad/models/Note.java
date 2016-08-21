62f137c838f5abc3d57ccd8423c5270beda7c413
Moh Mah <mohammadmahtabi@gmail.com>
2016-08-21 17:46:40 +0430

    removed Parcelable implementation from Note model because it's members aren't Parcelable

--- app/src/main/java/ir/cafebazaar/notepad/models/Note.java
+++ app/src/main/java/ir/cafebazaar/notepad/models/Note.java
@@ -1,7 +1,5 @@
 package ir.cafebazaar.notepad.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.text.Spannable;
 import android.text.SpannableString;
 import com.commonsware.cwac.richtextutils.SpannableStringGenerator;
@@ -22,19 +20,8 @@ import org.xml.sax.SAXException;
  */
 @ModelContainer
 @Table(database = AppDatabase.class, allFields = true)
-public class Note extends BaseModel implements Parcelable{
+public class Note extends BaseModel{
 
-	public static final Creator<Note> CREATOR = new Creator<Note>(){
-		@Override
-		public Note createFromParcel(Parcel in){
-			return new Note(in);
-		}
-
-		@Override
-		public Note[] newArray(int size){
-			return new Note[size];
-		}
-	};
 	@PrimaryKey(autoincrement = true)
 	private int id;
 	private String title;
@@ -44,12 +31,6 @@ public class Note extends BaseModel implements Parcelable{
 	private Date lastModified;
 
 	public Note(){}
-
-	protected Note(Parcel in){
-		id = in.readInt();
-		title = in.readString();
-		body = in.readString();
-	}
 
 	public int getId(){
 		return id;
@@ -97,16 +78,6 @@ public class Note extends BaseModel implements Parcelable{
 
 	public void setLastModified(Date lastModified){
 		this.lastModified = lastModified;
-	}
-
-	@Override public int describeContents(){
-		return 0;
-	}
-
-	@Override public void writeToParcel(Parcel dest, int flags){
-		dest.writeInt(id);
-		dest.writeString(title);
-		dest.writeString(body);
 	}
 
 	public Spannable getSpannedBody(){
