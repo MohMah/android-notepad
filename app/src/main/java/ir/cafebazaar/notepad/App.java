3a2534681dfabf511b1d13482381789d21747354
Moh Mah <mohammadmahtabi@gmail.com>
2016-08-21 17:41:57 +0430

    Added job manager library to save drawings off UI thread

--- app/src/main/java/ir/cafebazaar/notepad/App.java
+++ app/src/main/java/ir/cafebazaar/notepad/App.java
@@ -2,6 +2,10 @@ package ir.cafebazaar.notepad;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+import com.birbit.android.jobqueue.JobManager;
+import com.birbit.android.jobqueue.config.Configuration;
+import com.birbit.android.jobqueue.log.CustomLogger;
 import com.facebook.stetho.Stetho;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
@@ -11,6 +15,7 @@ import com.raizlabs.android.dbflow.config.FlowManager;
  */
 public class App extends Application{
 	public static volatile Context CONTEXT;
+	public static JobManager JOB_MANAGER;
 
 	@Override
 	public void onCreate(){
@@ -18,5 +23,52 @@ public class App extends Application{
 		CONTEXT = getApplicationContext();
 		FlowManager.init(new FlowConfig.Builder(this).build());
 		Stetho.initializeWithDefaults(this);
+		configureJobManager();
+	}
+
+	private void configureJobManager(){
+		Configuration.Builder builder = new Configuration.Builder(this)
+				.customLogger(new CustomLogger(){
+					private static final String TAG = "JOBS";
+
+					@Override
+					public boolean isDebugEnabled(){
+						return true;
+					}
+
+					@Override
+					public void d(String text, Object... args){
+						Log.d(TAG, String.format(text, args));
+					}
+
+					@Override
+					public void e(Throwable t, String text, Object... args){
+						Log.e(TAG, String.format(text, args), t);
+					}
+
+					@Override
+					public void e(String text, Object... args){
+						Log.e(TAG, String.format(text, args));
+					}
+
+					@Override
+					public void v(String text, Object... args){
+
+					}
+				})
+				.maxConsumerCount(3)//up to 3 consumers at a time
+				.loadFactor(3)//3 jobs per consumer
+				.consumerKeepAlive(120);//wait 2 minute
+		//if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+		//	builder.scheduler(FrameworkJobSchedulerService.createSchedulerFor(this,
+		//			MyJobService.class), true);
+		//}else{
+		//	int enableGcm = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+		//	if (enableGcm == ConnectionResult.SUCCESS){
+		//		builder.scheduler(GcmJobSchedulerService.createSchedulerFor(this,
+		//				MyGcmJobService.class), true);
+		//	}
+		//}
+		JOB_MANAGER = new JobManager(builder.build());
 	}
 }
