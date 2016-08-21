5cc64a65b8dfc426510fa83604e6e934a0ec8331
Moh Mah <mohammadmahtabi@gmail.com>
2016-08-21 17:48:25 +0430

    Added SaveDrawingJob to save drawing in the DrawingActivity off UI thread

--- app/src/main/java/ir/cafebazaar/notepad/activities/note/DrawingActivity.java
+++ app/src/main/java/ir/cafebazaar/notepad/activities/note/DrawingActivity.java
@@ -3,9 +3,16 @@ package ir.cafebazaar.notepad.activities.note;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.github.gcacace.signaturepad.views.SignaturePad;
+import ir.cafebazaar.notepad.App;
 import ir.cafebazaar.notepad.R;
+import ir.cafebazaar.notepad.database.NotesDAO;
+import ir.cafebazaar.notepad.jobs.SaveDrawingJob;
 import ir.cafebazaar.notepad.models.Note;
+import ir.cafebazaar.notepad.utils.Utils;
 import se.emilsjolander.intentbuilder.Extra;
 import se.emilsjolander.intentbuilder.IntentBuilder;
 
@@ -13,12 +20,30 @@ import se.emilsjolander.intentbuilder.IntentBuilder;
 public class DrawingActivity extends AppCompatActivity{
 	private static final String TAG = "DrawingActivity";
 	@Extra
+	Integer noteId;
 	Note note;
+
+	@BindView(R.id.drawing_pad) SignaturePad drawingPad;
 
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_drawing);
 		DrawingActivityIntentBuilder.inject(getIntent(), this);
 		ButterKnife.bind(this);
+		note = NotesDAO.getNote(noteId);
+	}
+
+	@Override protected void onStop(){
+		super.onStop();
+		App.JOB_MANAGER.addJobInBackground(new SaveDrawingJob(drawingPad, note.getId()));
+	}
+
+	@Override protected void onStart(){
+		super.onStart();
+		try{
+			drawingPad.setSignatureBitmap(Utils.getImage(note.getDrawing().getBlob()));
+		}catch (Exception e){
+			Log.e(TAG, "onStart: ", e);
+		}
 	}
 }
